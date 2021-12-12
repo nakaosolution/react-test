b7fc7b4dfb8ae210f0bd6730dfdef510e00cab90
nakaosolution <49142213+nakaosolution@users.noreply.github.com>
2021-12-13 05:14:23 +0900

    propsまで

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert("hey");
-  const contentStyle = {
-    color: "blue",
-    fontsize: "18px"
-  };
+  // const contentStyle = {
+  //   color: "blue",
+  //   fontsize: "18px"
+  // };
   return (
     <>
       <h1 style={{ color: "red" }}>HELLOWORLD</h1>
-      <p style={contentStyle}>how are you?</p>
+      <ColorfulMessage color="pink">howareyou</ColorfulMessage>
+      <ColorfulMessage color="blue" message="HOW ARE YOU" />
       <button onClick={onClickButton}>Button</button>
     </>
   );
